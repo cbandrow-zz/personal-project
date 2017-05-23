131f10924db0a17612457ae0a37fdd4f84859707
Chris Bandrowsky <chris.bandrowsky@gmail.com>
2017-05-23 15:39:25 -0600

    changes to config and server for hot reload.

--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,30 @@ const webpack = require('webpack');
 module.exports = {
   devtool: '#source-map',
   context: __dirname,
-   entry: [
-     './app/index.js'
-   ],
-   output: {
-     path: './app',
-     filename: 'bundle.js',
-     publicPath: '/'
+   entry: {
+     index: ['./app/index.js', 'webpack-hot-middleware/client', 'webpack/hot/dev-server'],
    },
+   devtool: 'source-map',
+   target: 'web',
+
+   output: {
+     path: '/',
+     filename: 'bundle.js',
+     publicPath: 'http://localhost:3000/app/',
+   },
+
+   plugins: [
+        new webpack.HotModuleReplacementPlugin()
+  ],
+
    module: {
      loaders: [{
        test: /.jsx?$/,
-       loader: 'babel-loader',
+      //  loader: 'babel-loader',
+       loaders: [
+                'react-hot',
+                'babel?presets[]=react,presets[]=es2015'
+            ],
        include: path.join(__dirname, 'app'),
        exclude: /node_modules/,
        query: {
